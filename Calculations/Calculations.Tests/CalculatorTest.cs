36971d888023435dda490b16f619633104a82946
dlopes <dylananlopes@gmail.com>
2023-10-21 22:48:04 +0100

    Add project files.

---
+++ Calculations/Calculations.Tests/CalculatorTest.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+using Calculations;
+
+namespace Calculations.Tests
+{
+  public class CalculatorTest
+  {
+    [Fact]
+    public void Add_GivenTwoIntValues_ReturnsInt()
+    {
+      var calc = new Calculator();
+      Assert.Equal(3, calc.Add(1, 2));
+    }
+
+    [Fact]
+    public void AddDouble_GivenTwoDoubleValues_ReturnsDouble()
+    {
+      var calc = new Calculator();
+      // The 3rd argument is about decimal tolerance. In this case 
+      // we got '1', which leads to a round up value -> which is correct, so the test passes
+      Assert.Equal(3.6, calc.AddDouble(1.23, 2.34), 1);
+    }
+  }
+}
---
+++ Calculations/Calculations/Calculator.cs
@@ -0,0 +1,8 @@
+﻿namespace Calculations
+{
+  public class Calculator
+  {
+    public int Add (int x, int y) { return x + y; }
+    public double AddDouble (double x, double y) {  return x + y; }
+  }
+}
