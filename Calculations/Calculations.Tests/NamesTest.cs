6fdf66f3c8a67ac5bda1746f77744edc57f0d9b5
dlopes <dylananlopes@gmail.com>
2023-10-21 23:03:09 +0100

    Asserting string values

---
+++ Calculations/Calculations.Tests/NamesTest.cs
@@ -0,0 +1,36 @@
+﻿using Xunit;
+
+namespace Calculations.Tests
+{
+  public class NamesTest
+  {
+    [Fact]
+    public void MakeFullNameTest()
+    {
+      var names = new Names();
+      // There are some optional arguments to ignore some situations (in this case, we're using ignoreCase)
+      Assert.Equal("Dylan Lopes", names.MakeFullName("dylan", "Lopes"), ignoreCase: true);
+    }
+
+    [Fact]
+    public void MakeName_ContainsTest()
+    {
+      var names = new Names();
+      Assert.Contains("Dylan", names.MakeFullName("dylan", "Lopes"), StringComparison.InvariantCultureIgnoreCase);
+    }
+
+    [Fact]
+    public void MakeName_StartsWithTest()
+    {
+      var names = new Names();
+      Assert.StartsWith("Dylan", names.MakeFullName("dylan", "Lopes"), StringComparison.InvariantCultureIgnoreCase);
+    }
+
+    [Fact]
+    public void MakeName_RegexText()
+    {
+      var names = new Names();
+      Assert.Matches("[A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+", names.MakeFullName("Dylan", "Lopes"));
+    }
+  }
+}
