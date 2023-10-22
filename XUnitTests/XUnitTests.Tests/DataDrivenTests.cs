fe3951e51c8313ad1ff337ac29bb957eb13aa3a3
dlopes <dylananlopes@gmail.com>
2023-10-22 14:27:33 +0100

    Data driven -> External data

--- XUnitTests/XUnitTests.Tests/DataDrivenTests.cs
+++ XUnitTests/XUnitTests.Tests/DataDrivenTests.cs
@@ -12,5 +12,14 @@ namespace XUnitTests.Tests
       var obj = new DataDriven();
       Assert.Equal(expected, obj.IsOdd(value));
     }
+
+    [Theory]
+    [Trait("Category", "DataDriven")]
+    [MemberData(nameof(TestDataShare.IsOddOrEvenExternalData), MemberType = typeof(TestDataShare))]
+    public void IsOdd_TestOddAndEven_ExternalData(int value, bool expected)
+    {
+      var obj = new DataDriven();
+      Assert.Equal(expected, obj.IsOdd(value));
+    }
   }
 }
--- XUnitTests/XUnitTests.Tests/TestDataShare.cs
+++ XUnitTests/XUnitTests.Tests/TestDataShare.cs
@@ -10,5 +10,18 @@
         yield return new object[] { 2, false };
       }
     }
+
+    public static IEnumerable<object[]> IsOddOrEvenExternalData
+    {
+      get
+      {
+        var allLines = File.ReadAllLines("C:\\Users\\dlopes\\Desktop\\Projects\\UnitTesting\\XUnitTests\\XUnitTests.Tests\\IsOddOrEvenTestData.txt");
+        return allLines.Select(line =>
+        {
+          var split = line.Split(',');
+          return new object[] { split[0], split[1] };
+        });
+      }
+    }
   }
 }
