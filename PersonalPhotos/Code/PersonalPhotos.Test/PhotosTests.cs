bb59e9601b650f4f7c0a8b677a27287e41eba02d
dlopes <dylananlopes@gmail.com>
2023-10-25 08:39:36 +0100

    Finished PhotoTests; Added LegacyCode;

---
+++ PersonalPhotos/Code/PersonalPhotos.Test/PhotosTests.cs
@@ -0,0 +1,40 @@
+﻿using Core.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.Web.CodeGeneration;
+using Moq;
+using PersonalPhotos.Controllers;
+using PersonalPhotos.Models;
+using System.Text;
+
+namespace PersonalPhotos.Test
+{
+  public class PhotosTests
+  {
+    [Fact]
+    public async Task Upload_GivenFileName_ReturnsDisplayAction()
+    {
+      // The controller method has an attribute that validates if the user is in this session
+      // Then we need to add it with Set
+      var session = Mock.Of<ISession>();
+      session.Set("user", Encoding.UTF8.GetBytes("a@test.com"));
+
+      var context = Mock.Of<HttpContext>(x => x.Session ==  session);
+      var accessor = Mock.Of<IHttpContextAccessor>(x => x.HttpContext == context);
+
+      var fileStorage = Mock.Of<IFileStorage>();
+      var keyGenerator = Mock.Of<IKeyGenerator>();
+      var photoMetadata = Mock.Of<IPhotoMetaData>();
+
+      var formFile = Mock.Of<IFormFile>();
+      var model = Mock.Of<PhotoUploadViewModel>(x => x.File == formFile);
+
+      var controller = new PhotosController(keyGenerator, accessor, photoMetadata, fileStorage);
+
+      var result = (await controller.Upload(model)) as RedirectToActionResult;
+
+      Assert.NotNull(result);
+      Assert.Equal("Display", result.ActionName, ignoreCase: true);
+    }
+  }
+}
