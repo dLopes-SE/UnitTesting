cd48a9d94fb6d0f6ea5c2b022b03a79c2f5060a8
dlopes <dylananlopes@gmail.com>
2023-10-26 22:18:51 +0100

    Add bongo project; add tests; add wwwroot to .gitignore

---
+++ Bongo/Bongo.DataAccess.Tests/StudyBookRepositoryTests.cs
@@ -0,0 +1,56 @@
+﻿using Bongo.DataAccess.Repository;
+using Bongo.Models.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bongo.DataAccess.Tests
+{
+  [Trait("Category", "StudyBookRepositoryTests")]
+  public class StudyBookRepositoryTests
+  {
+    private StudyRoomBooking studyRoomBooking_One;
+    private StudyRoomBooking studyRoomBooking_Two;
+
+    public StudyBookRepositoryTests()
+    {
+      studyRoomBooking_One = new StudyRoomBooking()
+      {
+        FirstName = "Dylan1",
+        LastName = "Lopes1",
+        Date = new DateTime(2023, 1, 1),
+        Email = "dylan1@test.com",
+        BookingId = 11,
+        StudyRoomId = 1
+      };
+
+      studyRoomBooking_Two = new StudyRoomBooking()
+      {
+        FirstName = "Dylan2",
+        LastName = "Lopes2",
+        Date = new DateTime(2023, 2, 2),
+        Email = "dylan2@test.com",
+        BookingId = 22,
+        StudyRoomId = 2
+      };
+    }
+
+    [Fact] 
+    public void SaveBook_Booking_One_CheckTheValuesFromDB()
+    {
+      // arrange (Mocking an inMemoryDatabase)
+      var options = new DbContextOptionsBuilder<ApplicationDbContext>().UseInMemoryDatabase(databaseName: "temp_Bongo").Options;
+
+      // act -> saving the book to the inMemoryDB
+      using var context = new ApplicationDbContext(options);
+      var repo = new StudyRoomBookingRepository(context);
+      repo.Book(studyRoomBooking_One);
+
+      // Assert -> verify if the booking was correctly inserted
+      Assert.True(studyRoomBooking_One.Equals(context.StudyRoomBookings.FirstOrDefault(x => x.StudyRoomId == studyRoomBooking_One.StudyRoomId)));
+    }
+  }
+}
---
+++ Bongo/Bongo.Models.Tests/DateInFutureAttributeTests.cs
@@ -0,0 +1,22 @@
+﻿using Bongo.Models.ModelValidations;
+
+namespace Bongo.Models.Tests
+{
+  [Trait("Category", "DateInFutureAttributeTests")]
+  public class DateInFutureAttributeTests
+  {
+    [Theory]
+    [InlineData(100)]
+    [InlineData(200)]
+    public void DateValidator_InputExpectedDate_DateValidity(int addSeconds)
+    {
+      Assert.True(new DateInFutureAttribute(() => DateTime.Now).IsValid(DateTime.Now.AddSeconds(addSeconds)));
+    }
+
+    [Fact]
+    public void DateInFutureAttribute_VerifyErrorMessage()
+    {
+      Assert.Equal("Date must be in the future", new DateInFutureAttribute(() => DateTime.Now).ErrorMessage);
+    }
+  }
+}
