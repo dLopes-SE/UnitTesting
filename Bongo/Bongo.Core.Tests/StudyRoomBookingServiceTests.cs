33bae4918d3be505cb9471ed0ef8b10ac1115261
dlopes <dylananlopes@gmail.com>
2023-10-30 09:04:39 +0000

    Started Core Tests

---
+++ Bongo/Bongo.Core.Tests/StudyRoomBookingServiceTests.cs
@@ -0,0 +1,28 @@
+﻿using Bongo.Core.Services;
+using Bongo.DataAccess.Repository.IRepository;
+using Moq;
+
+namespace Bongo.Core.Tests
+{
+  [Trait("Category", "StudyRoomBookingServiceTests")]
+  public class StudyRoomBookingServiceTests
+  {
+    private readonly Mock<IStudyRoomBookingRepository> _studyRoomBookingRepo;
+    private readonly Mock<IStudyRoomRepository> _studyRoomRepo;
+    private readonly StudyRoomBookingService _bookingService;
+
+    public StudyRoomBookingServiceTests()
+    {
+      _studyRoomBookingRepo = new Mock<IStudyRoomBookingRepository>();
+      _studyRoomRepo = new Mock<IStudyRoomRepository>();
+      _bookingService = new StudyRoomBookingService(_studyRoomBookingRepo.Object, _studyRoomRepo.Object);
+    }
+
+    [Fact]
+    public void GetAllBooking_InvokeMethod_CheckIfRepoIsCalled()
+    {
+      _bookingService.GetAllBooking();
+      _studyRoomBookingRepo.Verify(x => x.GetAll(null), Times.Once);
+    }
+  }
+}
--- Bongo/Bongo.Core/Services/StudyRoomService.cs
+++ Bongo/Bongo.Core/Services/StudyRoomService.cs
@@ -1,15 +1,11 @@
 ﻿using Bongo.Core.Services.IServices;
 using Bongo.DataAccess.Repository.IRepository;
 using Bongo.Models.Model;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Bongo.Core.Services
 {
-    public class StudyRoomService : IStudyRoomService
+  public class StudyRoomService : IStudyRoomService
     {
         private readonly IStudyRoomRepository _studyRoomRepository;
         public StudyRoomService(IStudyRoomRepository studyRoomRepository)
